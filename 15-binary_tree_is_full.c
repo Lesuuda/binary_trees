e890f4c309a1faac274b737f82d6caa6594eba0b
lesuuda <lesuudaljamani@gmail.com>
2024-02-29 22:08:23 +0300

    fixed betty errors for all the c files

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,12 +1,18 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_is_full - finds the sibling of a node.
+ *@tree: pointer to the node to find the sibling.
+ * Return: true or false
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    int left = 0, right = 0;
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    left = binary_tree_is_full(tree->left);
-    right = binary_tree_is_full(tree->right);
-    return ((left == 0  && right == 0) || (left && right));
+	int left = 0, right = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+	return ((left == 0  && right == 0) || (left && right));
 }
