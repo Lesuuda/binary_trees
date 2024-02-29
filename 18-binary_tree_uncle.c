093a16a99cdbd154f9b6dc085559cca07f5256fd
lesuuda <lesuudaljamani@gmail.com>
2024-02-29 22:14:01 +0300

    fixed checker error for 18-uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,21 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_sibling - finds the sibling of a node.
+ *@node: pointer to the node to find the sibling.
+ * Return: pointer to the sibling node.
+ */
+binary_tree_t *binary_tree_sibling_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->right == node)
+		return (node->parent->left);
+	else
+		return (node->parent->right);
+
+}
+
+/**
  * binary_tree_uncle - function that finds the uncle of a node.
  *@node: pointer to the node to find the uncle.
  * Return: pointer to the uncle node.
@@ -8,5 +24,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL)
 		return (NULL);
-	return (binary_tree_sibling(node->parent));
+	return (binary_tree_sibling_uncle(node->parent));
 }
